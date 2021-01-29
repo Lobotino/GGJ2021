84148309038c8358ff065357037bb95b36e2a6f6
Lobotino <Lobotino1@gmail.com>
2021-01-30 03:58:49 +0500

    РАБОЧИЕ ТЕНИ

---
+++ Assets/Scripts/ShadowsScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowsScript : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().receiveShadows = true;
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On ; 
+    }
+}
