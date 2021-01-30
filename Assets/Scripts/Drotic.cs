2301bf2f0c1b40f92883eaa48c7eb71b3a2f2bc4
Lobotino <Lobotino1@gmail.com>
2021-01-30 19:04:06 +0500

    drotics

---
+++ Assets/Scripts/Drotic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drotic : MonoBehaviour
+{
+    public int damage = 2;
+
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.tag.Equals("Drotic") && !other.gameObject.tag.Equals("Floors"))
+        {
+            if (other.tag.Equals("Player"))
+            {
+                var player = other.gameObject.GetComponent<PlayerController>();
+                if (player != null && !player.IsDead())
+                {
+                    player.Hurt(damage);
+                    Destroy(this);
+                }
+            }
+            else
+            {
+                if (_rigidbody2D != null)
+                {
+                    _rigidbody2D.velocity = new Vector2(0, 0);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DroticsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class DroticsController : MonoBehaviour
+{
+    public bool isLeft = true;
+    public int droticsCount = 8;
+    public float shotSpeedInSeconds = 0.2f;
+    public float shotPower = 10f;
+
+    void Start()
+    {
+        ShotDrotics();
+    }
+
+    public void ShotDrotics()
+    {
+        StartCoroutine(WaitAndShot());
+    }
+
+    private IEnumerator WaitAndShot()
+    {
+        for (int i = 0; i < droticsCount; i++)
+        {
+            yield return new WaitForSeconds(shotSpeedInSeconds);
+            var drotic = PhotonNetwork.Instantiate("drotic", transform.position, Quaternion.identity);
+            if (!isLeft)
+            {
+                drotic.transform.rotation = new Quaternion(0, 0, 180, 0);
+            }
+            drotic.GetComponent<Rigidbody2D>().AddForce(new Vector2(isLeft ? -shotPower : shotPower, 0), ForceMode2D.Impulse);
+        }
+    }
+}
